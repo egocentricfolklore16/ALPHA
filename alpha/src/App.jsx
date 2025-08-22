0b85fc9d33de1128773344a1f7c843859d52140e
Egocentricfolklore16 <bakarecaleb02@gmail.com>
2025-08-23 00:50:19 +0100

    changed the package files and tweaked the App and Sec2 components

--- alpha/src/App.jsx
+++ alpha/src/App.jsx
@@ -2,7 +2,7 @@ import Navigation from "./components/common/Navigation";
 import ClickSpark from "./components/common/ClickSpark";
 import Loader from "./components/common/Loader";
 import Hero from "./components/pages/Home/sections/Hero/Hero";
-import StatsSection from "./components/pages/Home/sections/section2/sec2";
+import StatsSection from "./components/pages/Home/sections/section2/StatsSection";
 function App() {
   return (
     <div className="h-[100vh]">
